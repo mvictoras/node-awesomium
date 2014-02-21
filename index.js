767d43b07667e9d410d770c04a206823e071ba27
Victor Mateevitsi <mvictoras@gmail.com>
2014-02-20 21:46:59 -0600

    Fixing width and height

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var binding = require('./build/Release/awesomium')
 
-var webBrowser = binding.WebBrowser("http://www.vimeo.com", 800, 800);
+var webBrowser = binding.WebBrowser("http://www.vimeo.com", 1366, 390);
 
 module.exports.getFrame = function() {
     return webBrowser.getFrame();
