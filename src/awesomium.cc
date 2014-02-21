ef0bcde5a9f3457366541b246d0b765fbdc109bb
Victor Mateevitsi <mvictoras@gmail.com>
2014-02-20 22:20:21 -0600

    Fixing width and height

--- src/awesomium.cc
+++ src/awesomium.cc
@@ -58,7 +58,7 @@ void WebBrowser::Init(Handle<Object> exports) {
     // Prepare constructor template
     Local<FunctionTemplate> tpl = FunctionTemplate::New(New);
     tpl->SetClassName(String::NewSymbol("WebBrowser"));
-    tpl->InstanceTemplate()->SetInternalFieldCount(7);
+    tpl->InstanceTemplate()->SetInternalFieldCount(5);
     // Prototype
     tpl->PrototypeTemplate()->Set(String::NewSymbol("plusOne"),
         FunctionTemplate::New(PlusOne)->GetFunction());
