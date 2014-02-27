8c7e64dc237fdfb6662b8d3b7fe764bc1c3b8ccc
Victor Mateevitsi <mvictoras@gmail.com>
2014-02-26 23:22:07 -0600

    Typo

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ module.exports.getFrame = function(id) {
     return webBrowser.getFrame(id);
 }
 
-module.exports.keypress = function(id, key) {
-    webBrowser.keypress(id, key);
+module.exports.keyPress = function(id, key) {
+    webBrowser.keyPress(id, key);
 }
 
 module.exports.click = function(id, x, y) {
