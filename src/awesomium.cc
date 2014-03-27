a41bb7e813e98fe97777b232b40e2e4bbf8e5594
Victor Mateevitsi <mvictoras@gmail.com>
2014-03-27 18:34:46 -0500

    cef3

--- src/awesomium.cc
+++ src/awesomium.cc
@@ -90,7 +90,7 @@ WebBrowser::WebBrowser(int wallWidth, int wallHeight, int initWidth, int initHei
 
     CefSettings settings;
     CefString(&settings.resources_dir_path).FromASCII("node_modules/node-awesomium/build/Release/Resources");
-    CefString(&settings.locales_dir_path).FromASCII("node_modules/node-awesomium/build/Release/Resources");
+    CefString(&settings.locales_dir_path).FromASCII("node_modules/node-awesomium/build/Release/Resources/locales");
     settings.single_process = true;
     settings.no_sandbox = true;
     settings.multi_threaded_message_loop = false;
