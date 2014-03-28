ce92ee22c8bb22ba2fd75631cbd131110a4b8a91
Victor Mateevitsi <mvictoras@gmail.com>
2014-03-27 19:58:10 -0500

    cef3

--- src/awesomium.cc
+++ src/awesomium.cc
@@ -93,11 +93,9 @@ WebBrowser::WebBrowser(int wallWidth, int wallHeight, int initWidth, int initHei
     CefSettings settings;
     CefString(&settings.resources_dir_path).FromASCII("node_modules/node-awesomium/build/Release");
     CefString(&settings.locales_dir_path).FromASCII("node_modules/node-awesomium/build/Release/locales");
-    settings.single_process = true;
-    settings.multi_threaded_message_loop = false;
     CefInitialize(main_args, settings, NULL, NULL);
 
-    std::cout << "Test" << std::endl;
+    std::cerr << "Test" << std::endl;
 
     CefBrowserSettings browserSettings;
     CefWindowInfo window_info;
